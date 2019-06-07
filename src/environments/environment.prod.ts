8fc7b36c89f0a739a81533b46c75f71599199848
entropia.studio <info@entropia.studio>
2019-06-07 18:36:37 +0200

    Fix errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAy_zBKJp382ApU7llHKpTGNwwLziEWD_0",
+    authDomain: "fitness-app-7db7c.firebaseapp.com",
+    databaseURL: "https://fitness-app-7db7c.firebaseio.com",
+    projectId: "fitness-app-7db7c",
+    storageBucket: "fitness-app-7db7c.appspot.com",
+    messagingSenderId: "35672455252",
+    appId: "1:35672455252:web:258926cb7829561c"
+  }
 };
