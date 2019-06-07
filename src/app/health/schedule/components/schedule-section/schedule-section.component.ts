6cee97fae1ffa3d13f539ebbc25ba8367e6a50d2
entropia.studio <info@entropia.studio>
2019-06-07 12:04:04 +0200

    Finish Rendering schedule sections from Observables

--- src/app/health/schedule/components/schedule-section/schedule-section.component.ts
+++ src/app/health/schedule/components/schedule-section/schedule-section.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ScheduleItem } from 'src/app/health/shared/services/schedule/schedule.service';
 
 @Component({
   selector: 'schedule-section',
   templateUrl: './schedule-section.component.html',
   styleUrls: ['./schedule-section.component.scss']
 })
-export class ScheduleSectionComponent implements OnInit {
+export class ScheduleSectionComponent {
 
-  constructor() { }
+  @Input()
+  name: string;
 
-  ngOnInit() {
-  }
+  @Input()
+  section: ScheduleItem;
+
+  @Output()
+  select = new EventEmitter<any>();
+
+  onSelect(type: string, assigned: string[] = []) {
+    const data = this.section;
+    this.select.emit({
+      type,
+      assigned,
+      data
+    });
+}
 
 }
